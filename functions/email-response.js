8afef410bd3d02b31a5df06f42c02b3a5f11d2c4
Robert Bridgeman <rob.bridgeman@gmail.com>
2019-06-25 21:58:51 -0700

    form updates netlify

--- functions/email-response.js
+++ functions/email-response.js
@@ -1,69 +1,69 @@
 
 exports.handler = function(event, context, callback) {
+  require('dotenv').config({path: path.join(__dirname, '.env')})    
+  var nodemailer = require("nodemailer");
+  var emailPassword = process.env.DB_PASS
 
-    var path = require("path");
+  console.log("FROM SERVER: ", output)
 
-    require('dotenv').config({path: path.join(__dirname, '.env')})
-    var emailPassword = process.env.DB_PASS
-
-    var nodemailer = require("nodemailer");
-
-    const { name, email, company, message } = JSON.parse(event.body);
-
-
-    app.post('/email-response', (req, res) => {
-        const output = `
-          <h2>Contact Us Details</h
-          <hr style="margin-right:200px">
-          <ul>  
-            <li>Name: ${event.body.name}</li>
-            <br>
-            <li>Email: ${event.body.email}</li>
-            <br>
-            <li>Company: ${event.body.company}</li>
-            </ul>
-          <hr style="margin-right:200px">
-          <h2>Message</h2>
-          <p style="border: 2px solid steelblue; padding:25px; margin-right:200px">${event.body.message}</p>
-        `;
+  const payload = JSON.parse(event.body).payload;
+  const { name, email, company, message } = payload;
       
-        console.log("FROM SERVER: ", output)
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      service: 'gmail',
+      port: 465,
+      secure: true, // true for 465, false for other ports
+      auth: {
+          user: 'rob.bridgeman@gmail.com', 
+          pass: emailPassword
+      },
+      tls:{
+        rejectUnauthorized:false
+      }
+    });
       
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-          host: 'smtp.gmail.com',
-          service: 'gmail',
-          port: 465,
-          secure: true, // true for 465, false for other ports
-          auth: {
-              user: 'rob.bridgeman@gmail.com', 
-              pass: emailPassword
-          },
-          tls:{
-            rejectUnauthorized:false
-          }
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: data.email, // sender address
+        to: '"Robert Bridgeman", <rob.bridgeman@gmail.com>', // list of receivers
+        subject: `Job Opportunity from ${data.company}`, // Subject line
+        html: `
+        <h2>Contact Us Details</h
+        <hr style="margin-right:200px">
+        <ul>  
+          <li>Name: ${data.name}</li>
+          <br>
+          <li>Email: ${data.email}</li>
+          <br>
+          <li>Company: ${data.company}</li>
+          </ul>
+        <hr style="margin-right:200px">
+        <h2>Message</h2>
+        <p style="border: 2px solid steelblue; padding:25px; margin-right:200px">${data.message}</p>
+      `  // html body
+    };
+    console.log(mailOptions);
+    // send mail with defined transport object
+    console.log("Sending Mail server side!");
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      // handle errors
+      if (error) {
+        return callback(null, {
+          statusCode: 500,
+          body: JSON.stringify(error)
         });
-      
-        // setup email data with unicode symbols
-        let mailOptions = {
-            from: event.body.email, // sender address
-            to: '"Robert Bridgeman", <rob.bridgeman@gmail.com>', // list of receivers
-            subject: `Job Opportunity from ${event.body.company}`, // Subject line
-            html: output // html body
-        };
-        console.log(mailOptions);
-        // send mail with defined transport object
-        console.log("Sending Mail server side!");
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                return console.log(error);
-            }
-            console.log('Mail sent successfully! ! !')
-            console.log('Message sent: %s', info.messageId);   
-            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-      
-        });
+      }
+  
+      // success!
+      callback(null, {
+        statusCode: 200,
+        body: "mail sent"
       });
+
+    });
 
     
 }
